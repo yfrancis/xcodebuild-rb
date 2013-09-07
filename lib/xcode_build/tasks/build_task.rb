e2f2c712b080c2d38e9c0545c82acc3d18fd31ba
Youssef Francis <youssef.francis@airbnb.com>
2013-09-06 17:42:19 -0700

    merge 0.3.0 and build dir patch

--- lib/xcode_build/tasks/build_task.rb
+++ lib/xcode_build/tasks/build_task.rb
@@ -18,6 +18,7 @@ module XcodeBuild
       attr_accessor :invoke_from_within
       attr_accessor :reporter_klass
       attr_accessor :xcodebuild_log_path
+      attr_accessor :build_dir
 
       def initialize(namespace = :xcode, &block)
         @namespace = namespace
@@ -25,21 +26,23 @@ module XcodeBuild
         @invoke_from_within = "."
         @reporter_klass = XcodeBuild::Reporter
         @hooks = {}
-        @build_settings = {}
-
+        
         yield self if block_given?
         define
       end
       
-      [:before, :after].each do |prefix|
-        [:build, :clean, :archive].each do |task|
-          hook_name = "#{prefix}_#{task}"
-          define_method(hook_name) do |&block|
-            set_hook hook_name, block
-          end
-        end
+      def after_build(&block)
+        @hooks[:after_build] = block
       end
-
+      
+      def after_clean(&block)
+        @hooks[:after_clean] = block
+      end
+      
+      def after_archive(&block)
+        @hooks[:after_archive] = block
+      end
+      
       def execute_hook(name, *args)
         if hook = @hooks[name.to_sym]
           hook.call(*args)
@@ -60,43 +63,20 @@ module XcodeBuild
           opts << "-arch #{arch}" if arch
           opts << "-sdk #{sdk}" if sdk
           opts << "-xcconfig #{xcconfig}" if xcconfig
-
-          @build_settings.each do |setting, value|
-            opts << "#{setting}=#{value}"
-          end
+          opts << "CONFIGURATION_BUILD_DIR=\"#{build_dir}\"" if build_dir
         end
       end
-
-      def add_build_setting(setting, value)
-        @build_settings[setting] = value
-      end
-
+      
       def reporter
         @reporter ||= @reporter_klass.new(formatter)
-      end
-      
-      def formatter=(formatter)
-        if formatter.is_a?(Symbol)
-          @formatter = XcodeBuild::Formatters[formatter]
-        else
-          @formatter = formatter
-        end
-      end
-      
-      def xcode_build_settings
-        Dir.chdir(invoke_from_within) { XcodeBuild.build_settings(build_opts_string) }
       end
 
       private
       
-      def set_hook(name, block)
-        @hooks[name.to_sym] = block
-      end
-
       def output_buffer
         @output_buffer ||= XcodeBuild::OutputTranslator.new(reporter)
       end
-
+      
       def build_opts_string(*additional_opts)
         (build_opts + additional_opts).compact.join(" ")
       end
@@ -104,30 +84,20 @@ module XcodeBuild
       def xcodebuild(action)
         reporter.direct_raw_output_to = output_to unless formatter
         reporter.direct_raw_output_to = File.open(xcodebuild_log_path, 'w') if xcodebuild_log_path
-
+        
         reporter.report_running_action(action) if reporter.respond_to?(:report_running_action)
         
-        case action
-        when :build
-          execute_hook(:before_build)
-        when :archive
-          execute_hook(:before_build)
-          execute_hook(:before_archive)
-        when :clean
-          execute_hook(:before_build)
-        end
-
         status = Dir.chdir(invoke_from_within) do
           XcodeBuild.run(build_opts_string(action), output_buffer)
         end
-
+        
         check_status(status)
-
+        
         if reporter.build && reporter.build.failed?
           # sometimes, a build/archive can fail and xcodebuild won't return a non-zero code
           raise "xcodebuild failed (#{reporter.build.failed_steps.length} steps failed)"
         end
-
+        
         case action
         when :build
           execute_hook(:after_build, reporter.build)
@@ -138,7 +108,7 @@ module XcodeBuild
           execute_hook(:after_clean, reporter.clean)
         end
       end
-      
+
       def define
         namespace(@namespace) do
           desc "Creates an archive build of the specified target(s)."
@@ -151,35 +121,17 @@ module XcodeBuild
           task :build do
             xcodebuild :build
           end
-
+          
           desc "Cleans the build using the same build settings."
           task :clean do
             xcodebuild :clean
           end
-
-          desc "Builds and installs the target"
-          task :install do
-            xcodebuild :install
-          end
-
+          
           desc "Builds the specified target(s) from a clean slate."
           task :cleanbuild => [:clean, :build]
-          
-          desc "Prints the full Xcode build settings"
-          task :settings do
-            puts "Build settings for #{build_opts_string}:"
-            xcode_build_settings.each do |target, settings|
-              header = "Target: #{target}"
-              puts header.length.times.map { "=" }.join
-              puts header
-              puts header.length.times.map { "=" }.join
-              puts settings.map { |key, val| "#{key} = #{val}" }.join("\n")
-              puts
-            end
-          end
         end
       end
-
+      
       def check_status(status)
         raise "xcodebuild failed (exited with status: #{status})" unless status == 0
       end
